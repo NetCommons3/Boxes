42c4a50892b477190e93580b8dfb8cbcf92f9549
s-nakajima <nakajimashouhei@gmail.com>
2015-07-07 22:45:36 +0900

    Add recodes

--- Config/Migration/1434983278_init.php
+++ Config/Migration/1434983278_init.php
@@ -76,6 +76,94 @@ e.g.) ルーム管理者、またはそれに準ずるユーザ: ルーム管理
 	);
 
 /**
+ * Records keyed by model name.
+ *
+ * @var array $records
+ */
+	public $records = array(
+		'Box' => array(
+			array(
+				'id' => '1',
+				'container_id' => '1',
+				'type' => '1',
+				'space_id' => '1',
+				'room_id' => null,
+				'page_id' => null,
+				'weight' => '1',
+			),
+			array(
+				'id' => '2',
+				'container_id' => '2',
+				'type' => '1',
+				'space_id' => '1',
+				'room_id' => null,
+				'page_id' => null,
+				'weight' => '1',
+			),
+			array(
+				'id' => '3',
+				'container_id' => '3',
+				'type' => '4',
+				'space_id' => null,
+				'room_id' => 1,
+				'page_id' => '1',
+				'weight' => '1',
+			),
+			array(
+				'id' => '4',
+				'container_id' => '4',
+				'type' => '1',
+				'space_id' => 1,
+				'room_id' => null,
+				'page_id' => null,
+				'weight' => '1',
+			),
+			array(
+				'id' => '5',
+				'container_id' => '5',
+				'type' => '1',
+				'space_id' => 1,
+				'room_id' => null,
+				'page_id' => null,
+				'weight' => '1',
+			),
+		),
+
+		'BoxesPage' => array(
+			array(
+				'id' => '1',
+				'page_id' => '1',
+				'box_id' => '1',
+				'is_published' => true,
+			),
+			array(
+				'id' => '2',
+				'page_id' => '1',
+				'box_id' => '2',
+				'is_published' => true,
+			),
+			array(
+				'id' => '3',
+				'page_id' => '1',
+				'box_id' => '3',
+				'is_published' => true,
+			),
+			array(
+				'id' => '4',
+				'page_id' => '1',
+				'box_id' => '4',
+				'is_published' => true,
+			),
+			array(
+				'id' => '5',
+				'page_id' => '1',
+				'box_id' => '5',
+				'is_published' => true,
+			),
+		),
+	);
+
+/**
  * Before migration callback
  *
  * @param string $direction Direction of migration process (up or down)
@@ -92,6 +180,36 @@ e.g.) ルーム管理者、またはそれに準ずるユーザ: ルーム管理
  * @return bool Should process continue
  */
 	public function after($direction) {
+		if ($direction === 'down') {
+			return true;
+		}
+
+		foreach ($this->records as $model => $records) {
+			if (!$this->updateRecords($model, $records)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+/**
+ * Update model records
+ *
+ * @param string $model model name to update
+ * @param string $records records to be stored
+ * @param string $scope ?
+ * @return bool Should process continue
+ */
+	public function updateRecords($model, $records, $scope = null) {
+		$Model = $this->generateModel($model);
+		foreach ($records as $record) {
+			$Model->create();
+			if (!$Model->save($record, false)) {
+				return false;
+			}
+		}
+
 		return true;
 	}
 }
