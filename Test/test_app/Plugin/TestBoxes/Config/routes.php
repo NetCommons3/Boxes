0c3c1da1a871c239e98141c347ca0c01569f5707
s-nakajima <nakajimashouhei@gmail.com>
2016-07-19 00:03:45 +0900

    ルーティングの見直し https://github.com/NetCommons3/NetCommons3/issues/309 https://github.com/NetCommons3/NetCommons/pull/284

---
+++ Test/test_app/Plugin/TestBoxes/Config/routes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * テストBoxesのルーティング
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+App::uses('Current', 'NetCommons.Utility');
+
+Router::connect(
+	'/' . Current::SETTING_MODE_WORD . '/test_boxes/:controller/:action/*',
+	array('plugin' => 'test_boxes')
+);
+
+Router::connect(
+	'/test_boxes/test_boxes/:action/*',
+	array('plugin' => 'test_boxes', 'controller' => 'test_boxes')
+);
