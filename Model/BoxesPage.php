5b4a43f3e924ebd496f066edd398c164b2d82fae
s-nakajima <nakajimashouhei@gmail.com>
2016-11-09 17:50:51 +0900

    @codeCoverageIgnore追加

--- Model/BoxesPage.php
+++ Model/BoxesPage.php
@@ -2,6 +2,9 @@
 /**
  * BoxesPage Model
  *
+ * このファイルは不要になったため、カバレッジから除外する。
+ * 後々、削除する予定
+ *
  * @property Page $Page
  * @property Box $Box
  *
@@ -11,10 +14,14 @@
  * @license http://www.netcommons.org/license.txt NetCommons License
  */
 
+//@codeCoverageIgnoreStart
 App::uses('PagesAppModel', 'Pages.Model');
+//@codeCoverageIgnoreEnd
 
 /**
  * Summary for BoxesPage Model
+ *
+ * @codeCoverageIgnore
  */
 class BoxesPage extends PagesAppModel {
 
