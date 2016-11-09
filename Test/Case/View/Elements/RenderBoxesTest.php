f794ea6219060459d78f21e9957ee8628cf97e1d
s-nakajima <nakajimashouhei@gmail.com>
2016-11-09 18:18:05 +0900

    View/Elements/render_boxesのテスト

---
+++ Test/Case/View/Elements/RenderBoxesTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * View/Elements/render_boxesのテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * View/Elements/render_boxesのテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Boxes\Test\Case\View\Elements\RenderBoxes
+ */
+class BoxesViewElementsRenderBoxesTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array();
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'boxes';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//テストプラグインのロード
+		NetCommonsCakeTestCase::loadTestPlugin($this, 'Boxes', 'TestBoxes');
+		//テストコントローラ生成
+		$this->generateNc('TestBoxes.TestViewElementsRenderBoxes');
+	}
+
+/**
+ * View/Elements/render_boxesのテスト
+ *
+ * @return void
+ */
+	public function testRenderBoxes() {
+		//テスト実行
+		$this->_testGetAction('/test_boxes/test_view_elements_render_boxes/render_boxes',
+				array('method' => 'assertNotEmpty'), null, 'view');
+
+		//チェック
+		$pattern = '/' . preg_quote('View/Elements/render_boxes', '/') . '/';
+		$this->assertRegExp($pattern, $this->view);
+
+		//TODO:必要に応じてassert追加する
+		var_export($this->view);
+	}
+
+}
